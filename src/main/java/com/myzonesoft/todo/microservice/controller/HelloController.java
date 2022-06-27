af193d6e617fdbefb3753f4839a69e307c8da687
udbalasooriya <udbalasooriya@virtusa.com>
2022-06-28 05:24:31 +0530

    added HelloController.java

---
+++ src/main/java/com/myzonesoft/todo/microservice/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.myzonesoft.todo.microservice.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = { "http://localhost:3000", "http://localhost:4200" })
+@RequestMapping("/hello")
+@RestController
+public class HelloController {
+
+    @GetMapping
+    public String getAllTasks() {
+        return "Hello World!!!";
+    }
+
+}
